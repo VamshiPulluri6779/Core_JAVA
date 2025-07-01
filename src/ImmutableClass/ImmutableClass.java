939432d943dc0b639f263227601d7f23a3b4e7bc
VamshiPulluri6779 <97658728+VamshiPulluri6779@users.noreply.github.com>
2025-07-01 15:56:53 +0530

    Update ImmutableClass.java

--- src/ImmutableClass/ImmutableClass.java
+++ src/ImmutableClass/ImmutableClass.java
@@ -3,14 +3,15 @@ package ImmutableClass;
 import java.util.ArrayList;
 import java.util.List;
 
-// Immutable class is one whose state can't be changes
-// Make this class final so that it can't be extended
+// Immutable class is one whose state can't be changed
+// Make this class final so that it can't be extended by child classes
 public final class ImmutableClass {
 
-    // Once initialized this variable can't be changed, hence made final
+    // Once initialized this variable shouldn't be changed, hence made final
     private final String name;
     private final List<String> items;
 
+    // Constructor for setting the values of the fields
     ImmutableClass(String name, List<String> items) {
         this.name = name;
         this.items = items;
@@ -25,5 +26,11 @@ public final class ImmutableClass {
     // Hence, whenever this method is called, we send a copy of list
     public List<String> getItems() {
         return new ArrayList<>(items);
+    }
+
+    public static void main(String[] args) {
+        ImmutableClass immutableClass = new ImmutableClass("Immutable", List.of("Item1", "Item2"));
+
+        System.out.println(immutableClass.getItems());
     }
 }
