19d93319042e99c253885e1a425308aa244b4b1e
VamshiPulluri6779 <97658728+VamshiPulluri6779@users.noreply.github.com>
2025-07-01 14:35:06 +0530

    Update CustomException.java

--- src/ExceptionHandling/CustomException.java
+++ src/ExceptionHandling/CustomException.java
@@ -1,5 +1,6 @@
 package ExceptionHandling;
 
+/* Any exception that extends Excpetion class is a checked and the ones which extend RuntimeException are unchecked ones*/
 public class CustomException extends Exception{
 
     CustomException(String message){
